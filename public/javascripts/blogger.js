2c854d21d459efe3d12ed44a6e7b772308f62048
Rayok <hs960411@gmail.com>
2021-09-27 21:47:53 +0800

    update js and css

--- public/javascripts/blogger.js
+++ public/javascripts/blogger.js
@@ -18,3 +18,29 @@ function endedPlay(ev){
     btn.classList.add('bi-play-fill');
     btn.classList.remove('active');
 }
+function switchWord(ev){
+    const th=ev;
+    const tw=th.nextElementSibling;
+    const audio1=tw.nextElementSibling;
+    const audio2=audio1.nextElementSibling
+
+    th.children[0].classList.toggle('d-none');
+    th.children[1].classList.toggle('d-none');
+    tw.children[0].classList.toggle('d-none');
+    tw.children[1].classList.toggle('d-none');
+    audio1.classList.toggle('d-none');
+    audio2.classList.toggle('d-none');
+}
+function switchWord2(ev){
+    const tw=ev;
+    const th=tw.previousElementSibling;
+    const audio1=tw.nextElementSibling;
+    const audio2=audio1.nextElementSibling
+
+    th.children[0].classList.toggle('d-none');
+    th.children[1].classList.toggle('d-none');
+    tw.children[0].classList.toggle('d-none');
+    tw.children[1].classList.toggle('d-none');
+    audio1.classList.toggle('d-none');
+    audio2.classList.toggle('d-none');
+}
