67ee4b7952f7e5322944768fbf30b931498b5e36
Rayok <hs960411@gmail.com>
2021-11-03 14:42:15 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var firebaseDb = require('../connections/firebase_admin_connect');
+var db = require('../connections/firebase_admin_connect');
 
-const docRef = firebaseDb.collection('users').doc('alovelace');
-
+const docRef = db.collection('users').doc('alovelace');
+const aTuringRef = db.collection('users').doc('aturing');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   docRef.set({
     first: 'Ada',
     last: 'Lovelace',
     born: 1815
+  });
+  aTuringRef.set({
+    'first': 'Alan',
+    'middle': 'Mathison',
+    'last': 'Turing',
+    'born': 1912
   })
   res.render('index', { title: 'Express AAA' });
 });
