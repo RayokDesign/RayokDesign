de96918fdd16f81e038117769eebbd7d1d2cbfdd
Rayok <hs960411@gmail.com>
2021-11-03 13:49:18 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
 var router = express.Router();
-var firebaseDb = require('../connections/firebase_admin_connect')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
