4ed7c84d0f23b7878a3ec5e0b358de1993882501
Rayok <hs960411@gmail.com>
2021-11-08 13:08:18 +0800

    update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false }
 }))
 /* ----- Rayok */
 // view engine setup
