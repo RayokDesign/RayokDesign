b98da7567a6bba3a37f9c478013b806ce58a51ab
Rayok <hs960411@gmail.com>
2021-12-31 22:04:06 +0700

    version 2.0

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -17,58 +17,110 @@ router.get('/', async function(req, res) {
     const data = await getRecords(db, date[0], date[1]);
     const uid = req.session.uid;
     const categories = await getCategories(db);
-    
+    const items = await getItems(db);
+
     res.render('restaurant', {
         title:'restaurant',
         moment: moment,
         uid: uid,
         categories: categories,
+        items: items,
         date: date,
         data: data
     });
 });
-
+router.get('/deleteitem', async function(req, res){
+    let deleteData= req.query.deleteitem.split('-');
+    let dayRef = db.doc(`restaurant/${deleteData[0]}/months/${deleteData[1]}/days/${deleteData[2]}`);
+    let deleteItemJson = `{
+        "records.${deleteData[3]}.items.${deleteData[4]}": FieldValue.delete() 
+    }`
+    await dayRef.update(eval('('+deleteItemJson+')'));
+    
+    let data = await dayRef.get();
+    let itemsEmptyCheck = Object.keys(data.data().records[deleteData[3]].items);
+    let deleteCategoryJson = `{
+        "records.${deleteData[3]}": FieldValue.delete() 
+    }`
+    if (itemsEmptyCheck.length == 0){
+        await dayRef.update(eval('('+deleteCategoryJson+')'));
+    }
+    // 當沒有任何紀錄時，刪除當天。
+    // data = await dayRef.get();
+    // let recordsEmptyCheck = Object.keys(data.data().records);
+    // if (recordsEmptyCheck.length == 0){
+    //     await dayRef.delete();
+    // }
+    res.redirect(`/restaurant?date=${deleteData[0]}-${deleteData[1]}`);
+})
 async function getRecords(db, year, month) {
     const daysRef = db.collection(`restaurant/${year}/months/${month}/days`);
-    const snapshot = await daysRef.orderBy("time", "desc").get();
+    const snapshot = await daysRef.get();
 
     let data = {};
     snapshot.forEach(doc => {
-        // if (Object.keys(doc.data().records).length != 0){
-        //     data[record.id] = data.data();
-        // }
         data[doc.id] = doc.data();
     });
-    console.log(data);
     return data;
 }
 
 router.post('/', async function(req, res) {
     await setDocument(db, req.body);
-    res.redirect('/restaurant');
+    res.redirect(`/restaurant?date=${req.query.date}`);
 });
 
 async function setDocument(db, data) {
     const date = data.date.split('-');
     const dayRef = db.doc(`restaurant/${date[0]}/months/${date[1]}/days/${date[2]}`);
-    const time = new Date().getTime();
+    const doc = await dayRef.get();
+    if (!doc.exists) {
+        let record = `{
+            "records": {
+                "0": {
+                    "category": "${data.category}",
+                    "items": {
+                        "0": {
+                            "item": "${data.item}",
+                            "amount": ${data.amount},
+                            "expin": "${data.expin}"
+                        }
+                    }
+                }
+            },
+            "categoryIndex": 0,
+            "itemIndex": 0,
+            "date": Timestamp.fromDate(new Date(data.date)),
+        }`;
+        await dayRef.set(eval('('+record+')'), {merge: true});
+    } else {
+        let docData = doc.data();
+        let haveCategory = false;
+        let categoryIndex = null;
 
-    let record = `{
-        "records.${data.category}.${data.item}.amount": ${data.amount},
-        "records.${data.category}.${data.item}.expin": "${data.expin}"
-    }`;
-    console.log(record);
-    await dayRef.set({
-        time: new Date(date[0], date[1]-1, date[2]).getTime()
-    }, { merge: true });
-    await dayRef.update(eval('('+record+')'));
-}
+        for (index in docData.records){
+            if (docData.records[index].category == data.category){
+                haveCategory = true;
+                categoryIndex = index;
+            }
+        }
+        if (haveCategory != true){
+            docData.categoryIndex += 1;
+            categoryIndex = docData.categoryIndex;
+        }
+        docData.itemIndex += 1;
+        let record = `{
+            "records.${categoryIndex}.category": "${data.category}",
+            "records.${categoryIndex}.items.${docData.itemIndex}": {
+                "item": "${data.item}",
+                "amount": ${data.amount},
+                "expin": "${data.expin}"
+            },
+            "categoryIndex": ${docData.categoryIndex},
+            "itemIndex": ${docData.itemIndex}
+        }`;
+        await dayRef.update(eval('('+record+')'));
+    }
 
-async function updateDocument(db, req) {
-  let memoDate = req.memoDate.split('-');
-  const recordRef = db.collection('restaurant').doc(memoDate[0]).collection(memoDate[1]).doc(memoDate[2]);
-
-  await recordRef.update({memo: req.memo});
 }
 
 router.get('/delete/:id', async function(req, res) {
@@ -86,21 +138,36 @@ async function deleteRecord(db, id) {
     await recordRef.update(eval('('+data+')'));
 }
 
-router.get('/categories', async function(req, res) {
+router.get('/manage', async function(req, res) {
     const categoriesRef = db.collection('categories');
-    const snapshot = await categoriesRef.orderBy('time').get();
-    let data = {}
-    snapshot.forEach(doc => {
-      data[doc.id] = doc.data();
+    const itemsRef = db.collection('items');
+    const categoriesSnapshot = await categoriesRef.orderBy('index').get();
+    const itemsSnapshot = await itemsRef.orderBy('index').get();
+    let categoriesData = {}, itemsData = {}
+    categoriesSnapshot.forEach(categoryDoc => {
+        categoriesData[categoryDoc.id] = categoryDoc.data();
     });
-    res.render('categories', {
-        title: 'categories',
-        data: data,
+    itemsSnapshot.forEach(itemDoc => {
+        itemsData[itemDoc.id] = itemDoc.data();
+    });
+    res.render('manage', {
+        title: 'manage',
+        categories: categoriesData,
+        items: itemsData
     })
 });
 async function getCategories(db) {
     const categoriesRef = db.collection('categories');
-    const snapshot = await categoriesRef.orderBy('time').get();
+    const snapshot = await categoriesRef.orderBy('index').get();
+    let categories = {}
+    snapshot.forEach(doc => {
+      categories[doc.id] = doc.data();
+    });
+    return categories;
+}
+async function getItems(db) {
+    const categoriesRef = db.collection('items');
+    const snapshot = await categoriesRef.orderBy('index').get();
     let categories = {}
     snapshot.forEach(doc => {
       categories[doc.id] = doc.data();
@@ -108,19 +175,39 @@ async function getCategories(db) {
     return categories;
 }
 
-router.post('/categories', async function(req, res) {
-    const categoriesRef = db.collection('categories');
-    await categoriesRef.add({
-        name: req.body.name,
-        time: new Date()
+router.get('/addoption', async function(req, res) {
+    let indexRef = db.doc(`${req.query.col}/index`);
+    let index = await indexRef.get();
+    await indexRef.update({
+        counter: FieldValue.increment(1)
     })
-    res.redirect('/restaurant/categories');
-});
+    console.log(index.data());
+    let dataRef = db.collection(`${req.query.col}`);
+    let addData = `{
+        index: ${index.data().counter},
+        name: "${req.query.name}"
+    }`
+    await dataRef.add(eval('('+addData+')'));
+    res.end();
+})
 
-router.get('/categories/delete/:id', async function(req, res) {
-    const id = req.params.id;
-    await db.doc(`categories/${id}`).delete();
-    res.redirect('/restaurant/categories');
-});
+router.get('/editoption', async function(req, res) {
+    let dataRef = db.doc(`${req.query.col}/${req.query.doc}`);
+    let updateData = `{
+        name: "${req.query.name}"
+    }`
+    await dataRef.update(eval('('+updateData+')'));
+    res.end();
+})
+
+router.get('/updatememo', async function(req, res) {
+    let date = (moment.unix(req.query.timestamp).format('YYYY-MM-DD').split('-'));
+    let dayRef = db.doc(`restaurant/${date[0]}/months/${date[1]}/days/${date[2]}`);
+
+    await dayRef.update({
+        memo: req.query.memo
+    })
+    res.end();
+})
 
 module.exports = router;
