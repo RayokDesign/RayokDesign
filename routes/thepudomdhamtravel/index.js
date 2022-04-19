44d72fdaae23b2cf5fdc565f2fd4a929c57a1da8
Rayok <hs960411@gmail.com>
2022-04-19 20:59:01 +0700

    'update'

--- routes/thepudomdhamtravel/index.js
+++ routes/thepudomdhamtravel/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('幹林老師');
   res.render('thepudomdhamtravel/index', {
     title: 'Thepudomthamtour'
   });
